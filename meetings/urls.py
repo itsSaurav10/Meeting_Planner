4a8b93a8e20103dfb5608955ca291604fca5f282
Saurav Mishra <131022973+itsSaurav10@users.noreply.github.com>
2023-05-07 14:17:52 +0530

    Add files via upload
    Here in this web application you can plan your new meeting.

---
+++ meetings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('<int:id>', views.detail, name="detail"),
+    path('rooms', views.rooms_list, name='rooms_list'),
+    path('form', views.form, name='form'),
+    path('start', views.start, name='start'),
+   
+]
